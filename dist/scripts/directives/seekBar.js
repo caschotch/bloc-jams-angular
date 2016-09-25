c906fe36f765af0e79418f392ebdb5dbd902cfe0
Craig Schotch <caschotchii@gmail.com>
2016-09-24 20:14:16 -0700

    Checkpoint 10 directives pt2

--- dist/scripts/directives/seekBar.js
+++ dist/scripts/directives/seekBar.js
@@ -14,12 +14,25 @@
             templateUrl: '/templates/directives/seek_bar.html',
             replace: true,
             restrict: 'E',
-            scope: { },
+            scope: {
+                onChange: '&'
+            },
             link: function(scope, element, attributes){
                 scope.value = 0;
                 scope.max = 100;
 
                 var seekBar = $(element);
+
+                // observes attributes declared in html by specificying the attribute name
+                attributes.$observe('value', function(newValue){
+                    scope.value = newValue;
+                });
+                // when attribute is set or changed this sets a new scope value
+                attributes.$observe('max', function(newValue){
+                    scope.max = newValue;
+                });
+
+
                 /*
                 * @function percentString
                 * converts percent number to string type
@@ -43,21 +56,30 @@
                 scope.onClickSeekBar = function(event) {
                     var percent = calculatePercent(seekBar, event);
                     scope.value = percent * scope.max;
+                    notifyOnChange(scope.value);
                 };
 
                 scope.trackThumb = function() {
                     $document.bind('mousemove.thumb', function(event){
                         var percent = calculatePercent(seekBar, event);
-                        scope.$appy(function(){
+                        scope.$apply(function() {
                             scope.value = percent * scope.max;
+                            notifyOnChange(scope.value);
                         });
                     });
 
-                    $document.bind('mouseup.thumb', function(){
-                        $document.unbind('mousemouve.thumb');
+                    $document.bind('mouseup.thumb', function() {
+                        $document.unbind('mousemove.thumb');
                         $document.unbind('mouseup.thumb');
                     });
                 };
+
+                var notifyOnChange = function(newValue) {
+                    if (typeof scope.onChange === 'function') {
+                        scope.onChange({value: newValue});
+                    }
+                };
+
             }
         };
     }
